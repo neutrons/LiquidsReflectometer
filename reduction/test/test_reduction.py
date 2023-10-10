3b1fa41f2d64a2ee2edc2802b181941f3e39e063
Mat Doucet <doucetm@ornl.gov>
2023-10-10 15:39:30 -0400

    Update test_reduction.py

--- reduction/test/test_reduction.py
+++ reduction/test/test_reduction.py
@@ -3,7 +3,7 @@ import os
 import mantid
 import mantid.simpleapi as mtd_api
 import numpy as np
-mtd_api.config.appendDataSearchDir('tests/data/liquidsreflectometer-data/nexus/')
+mtd_api.config.appendDataSearchDir('../tests/data/liquidsreflectometer-data/nexus/')
 mtd_api.config["default.facility"] = "SNS"
 mtd_api.config["default.instrument"] = "REF_L"
 
@@ -23,7 +23,7 @@ def test_full_reduction():
     d_refl_all = []
     first_run = None
     for run_number in range(198409, 198417):
-        ws_sc = mtd_api.LoadEventNexus("REF_L_%s.nxs.h5" % run_number)
+        ws_sc = mtd_api.Load("REF_L_%s.nxs.h5" % run_number)
         qz_mid, refl, d_refl = template.process_from_template_ws(ws_sc, template_path)
 
         if first_run is None:
