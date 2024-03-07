1336496113e78ac179c64e9c90d0ac25218af90d
Mathieu Doucet <doucetm@ornl.gov>
2024-03-07 09:16:41 -0500

    add deadtime algo

---
+++ reduction/test/test_dead_time.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+from lr_reduction.DeadTimeCorrection import SingleReadoutDeadTimeCorrection
+
+import mantid
+import mantid.simpleapi as mtd_api
+
+def test_deadtime():
+    """
+        Test the time-resolved reduction that uses a measured reference.
+        It is generally used at 30 Hz but it also works at 60 Hz.
+    """
+    ws = mtd_api.Load("REF_L_198409")
+
+    algo = SingleReadoutDeadTimeCorrection()
+    algo.PyInit()
+    algo.setProperty("InputWorkspace", ws)
+    algo.setProperty("OutputWorkspace", "dead_time_corr")
+    algo.PyExec()
+    corr_ws = algo.getProperty('OutputWorkspace').value
+    corr = corr_ws.readY(0)
+    for c in corr:
+        assert(c>0)
+        assert(c<1.001)
+
+def test_deadtime_paralyzable():
+    """
+        Test the time-resolved reduction that uses a measured reference.
+        It is generally used at 30 Hz but it also works at 60 Hz.
+    """
+    ws = mtd_api.Load("REF_L_198409")
+
+    algo = SingleReadoutDeadTimeCorrection()
+    algo.PyInit()
+    algo.setProperty("InputWorkspace", ws)
+    algo.setProperty("Paralyzable", True)
+    algo.setProperty("OutputWorkspace", "dead_time_corr")
+    algo.PyExec()
+    corr_ws = algo.getProperty('OutputWorkspace').value
+    corr = corr_ws.readY(0)
+    with open("dc.txt", 'w') as fd:
+        fd.write(str(corr))
+    for c in corr:
+        assert(c>0)
+        assert(c<1.001)
