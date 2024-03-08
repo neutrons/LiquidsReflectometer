b424ac06cd65e0765ad222172b948e2f609c5256
Mathieu Doucet <doucetm@ornl.gov>
2024-03-08 12:12:48 -0500

    add a test and clean up

--- reduction/test/test_dead_time.py
+++ reduction/test/test_dead_time.py
@@ -5,6 +5,7 @@ import mantid.simpleapi as mtd_api
 mtd_api.config["default.facility"] = "SNS"
 mtd_api.config["default.instrument"] = "REF_L"
 
+from lr_reduction import event_reduction, template, workflow
 from lr_reduction.utils import amend_config
 
 
@@ -43,8 +44,29 @@ def test_deadtime_paralyzable(nexus_dir):
     algo.PyExec()
     corr_ws = algo.getProperty('OutputWorkspace').value
     corr = corr_ws.readY(0)
-    with open("dc.txt", 'w') as fd:
-        fd.write(str(corr))
     for c in corr:
         assert(c>0)
         assert(c<1.001)
+
+def test_full_reduction(nexus_dir):
+    """
+        Test dead time from the reduction workflow
+    """
+    template_path = 'data/template.xml'
+    with amend_config(data_dir=nexus_dir):
+        ws = mtd_api.Load("REF_L_198409")
+
+    sequence_number = ws.getRun().getProperty("sequence_number").value[0]
+    template_data = template.read_template(template_path, sequence_number)
+    template_data.dead_time = True
+
+    _, r1, _ = template.process_from_template_ws(ws, template_data)
+
+    template_data.dead_time = False
+    _, r2, _ = template.process_from_template_ws(ws, template_data)
+
+    corr = r1/r2
+    for c in corr:
+        assert(c>0)
+        assert(c<1.001)
+
