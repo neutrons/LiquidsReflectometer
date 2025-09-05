c58c64766a9b72ae33eb9232e41f499fec3c6291
Kevin A. Tactac <tactacka@ornl.gov>
2025-09-05 15:52:51 -0400

    fix comparison test

--- reduction/tests/test_reduction.py
+++ reduction/tests/test_reduction.py
@@ -109,7 +109,7 @@ def test_full_reduction(nexus_dir):
     assert np.fabs(resolution - 0.02785205863936946) < 1e-5
     assert len(wavelength) == len(ws_sc.readY(1))
     assert len(d_lambda) == len(ws_sc.readY(2))
-    assert np.all(wavelength > 0)
+    assert np.all(np.array(wavelength) > 0)
     ref_data = np.loadtxt("data/reference_rq.txt").T
     assert len(ref_data[1]) == len(refl_all)
     assert np.fabs(np.sum(ref_data[1] - refl_all)) < 1e-10
