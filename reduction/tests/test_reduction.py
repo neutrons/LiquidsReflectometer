9c9313e821305d1f187982488ee31984b10a65b1
Kevin A. Tactac <tactacka@ornl.gov>
2025-09-08 12:41:01 -0400

    fix test

--- reduction/tests/test_reduction.py
+++ reduction/tests/test_reduction.py
@@ -107,8 +107,8 @@ def test_full_reduction(nexus_dir):
     d_refl_all = np.take_along_axis(d_refl_all, idx, axis=None)
 
     assert np.fabs(resolution - 0.02785205863936946) < 1e-5
-    assert len(wavelength) == len(ws_sc.readY(1))
-    assert len(d_lambda) == len(ws_sc.readY(2))
+    assert len(wavelength) == len(ws_sc.readX(1))
+    assert len(d_lambda) == len(ws_sc.readY(1))
     assert np.all(np.array(wavelength) > 0)
     ref_data = np.loadtxt("data/reference_rq.txt").T
     assert len(ref_data[1]) == len(refl_all)
