fixing direct beam calculation

src/lr_reduction/binary_processing.py
@@ -36,7 +36,7 @@ def convert_to_binary(fname, lowres, collapse_x = True, tofbin=50, tofmax=100000
     error_array_corr = np.nan_to_num(error_array_corr, nan=0)
 
     tof_array = tof_array / 1000
-    return tof_array, y_tof_corr, error_array_corr, log_values
+    return tof_array, y_tof_corr, error_array_corr, log_values, DTC
 
 def load_and_extract(fname):
     '''

src/lr_reduction/direct_beam_maker.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import os
 import numpy as np
 from matplotlib import pyplot as plt
+import nr_tools as tools
 
 class Direct_Beam:
 
@@ -16,7 +17,7 @@ class Direct_Beam:
             self.Cd_foils[1], 
             self.Cd_foils[1]+self.Cd_foils[2], 
             2*self.Cd_foils[1]+2*self.Cd_foils[2]] #microns Cd for each attenuator
-        self.Chop2_cut_fn = [2.077, -16818.0]        # linear fit to chopper cut time
+        self.Chop2_cut_fn = [2.077, -16.8180]        # linear fit to chopper cut time
         self.Icut = 1e-10                 # cut off any data below Icut (noisy)
         self.DTCcut = 1.25                # cut off any data above DTCcut (artifact-y)
         self.DTCcut_config1 = 1.5         # cut off any data above DTCcut_config1 for the first run
@@ -25,6 +26,8 @@ class Direct_Beam:
         self.t0=[0.114,0.0295]            # for a linear fit to emission time as a function of lambda: t0[0]+t0[1]*Lambda=emission time
         self.y_ROI = [130,170]            # y pixels to include in the direct beam spectrum
         self.low_res = [75,190]           # low res TOF range to include in the direct beam spectrum
+        self.n_y = 304
+        self.n_x = 256
 
     def create_db(self, run_list, save_name, plot=True, mu_file = 'Cd_mu_2025.dat', flip_atten=False, return_traces=False):
         # read in Cd linear attenuation coefficient data obtained from ENDf
@@ -35,6 +38,8 @@ class Direct_Beam:
         INT = []
         ERR = []
         traces = []
+        db_pixel = []
+        Cd_values = []
 
         if plot:
             plt.figure()
@@ -42,27 +47,35 @@ class Direct_Beam:
             # get header info from Nexus: atten and chop2 phase
             fname = self.NEXUSpath + 'REF_L_'+str(run)+'.nxs.h5'
 
-            e_offset, event_id, error_event_offset, pcharge, cPc, log_values = BP.load_and_extract(fname)
-            tof_array, DTC, error_counts = BP.get_deadtime_correction(error_event_offset, e_offset, cPc, 
-                                                                        tofbin=50, tofmax=100000, tofmin=0, deadtime=4.2, tof_step=100)
-            # Convert to other nomenclature for testing
-            tDTC = tof_array
-            DTC = DTC
-            tof_array, y_tof, error_array = BP.get_y_tof(tof_array, event_id, e_offset, self.low_res, pcharge, n_y=304, n_x=256)
-            T = tof_array
+            tof_array, y_tof_corr, error_array_corr, log_values, DTC_corr = BP.convert_to_binary(fname, self.low_res, collapse_x = True, tofbin=50, tofmax=100000, tofmin=0, deadtime=4.2, tof_step=100)
+            T = tof_array * 1000
+            DTC = DTC_corr
+            y_tof_corr = np.flipud(y_tof_corr) # flip the y-axis to match the orientation of the detector
+            error_array_corr = np.flipud(error_array_corr)
 
-            y_crop = y_tof[self.y_ROI[0]:self.y_ROI[1], :]
+            # Find the DB pixel
+            ypix = np.linspace(self.n_y-1, 0, self.n_y)
+            mask = (ypix >= self.y_ROI[0]+10) & (ypix < self.y_ROI[1]+10)
+            y_db = ypix[mask]
+            i_db = np.sum(y_tof_corr[mask,:], axis=1)
+            par, fit, bkg = tools.fit_peak(y_db, i_db, peaktype="supergauss", bkgtype="linear")
+            db_pixel.append(par[1])
+
+            y_crop = y_tof_corr[self.y_ROI[0]:self.y_ROI[1], :]
             I = np.sum(y_crop, axis=0)
-            E = np.sum(error_array[self.y_ROI[0]:self.y_ROI[1], :], axis=0)
+            E = np.sum(error_array_corr[self.y_ROI[0]:self.y_ROI[1], :], axis=0)
 
             # Need to split some parts out into separate functions if the logic is correct.
             Cd_thickness = self._extract_cd_values(log_values, flip_atten)
 
             #print some info for debugging
             print(f'Run {run}: Cd thickness = {Cd_thickness:.2f} cm')
+            Cd_values.append(Cd_thickness)
 
             ## This is the wrong shape. The I and E are 2 dimensional. Need to fix the output used above.
-            T, I, E, DTC = self._trim_and_chop(T, I, E, DTC, log_values["chop2_PD"], idx=i)
+            if run == run_list[-1]: low_tag = True
+            else: low_tag = False
+            T, I, E, DTC = self._trim_and_chop(T, I, E, DTC, log_values["chop2_PD"], lowest = low_tag)
 
             T = T * 1e-3 # convert to ms for easier handling
             #print some info for debugging
@@ -72,7 +85,6 @@ class Direct_Beam:
             # convert to Lambda
             L=3956*T/self.dMod
             L=3956*(T-(self.t0[0]+self.t0[1]*L))/self.dMod
-            print(L[2], T[2])
             
             mu = np.interp(L, L_ENDF, mu_ENDF)
             trans = np.exp(-mu*Cd_thickness)
@@ -104,11 +116,32 @@ class Direct_Beam:
             plt.yscale('log')
             plt.show()
 
+        db_pixel_mean, db_pixel_sigma, tthd_mean, tthd_sigma = self._calc_average_db_pixel(db_pixel, log_values["tthd"])
+
+        header = (
+            f"Processed DB spectrum from spectrum maker\n"
+            f"Cd attenuator = {Cd_values}\n"
+            f"db_runs = {run_list}\n"
+            f"db_pixel = {db_pixel_mean}\n"
+            f"db_pixel_err = {db_pixel_sigma}\n"
+            f"tthd = {tthd_mean}\n"
+            f"tthd_err = {tthd_sigma}\n"
+            "columns = lambda intensity error"
+        )
+
         array = np.column_stack((lam_out,int_out,err_out))
-        np.savetxt(self.savepath+save_name, array, header = 'Lambda\tIntensity\tError', delimiter='\t')
+        np.savetxt(self.savepath+save_name, array, header = header, delimiter='\t')
         if return_traces:
             return lam_out, int_out, err_out, traces
         return lam_out, int_out, err_out
+
+    def _calc_average_db_pixel(self, db_pixel_list, tthd):
+        MeanPos = np.round(np.mean(db_pixel_list),2)
+        SigmaPos = np.round(np.std(db_pixel_list),2)
+        MeanTTHD = np.round(np.mean(tthd),2)
+        SigmaTTHD = np.round(np.std(tthd),2)
+        print(f'Average DB pixel: {MeanPos} +/- {SigmaPos}')
+        return MeanPos, SigmaPos, MeanTTHD, SigmaTTHD
 
     def _lam_error_sort(self, LAM, INT, ERR):
         idx = np.argsort(LAM)
@@ -152,7 +185,7 @@ class Direct_Beam:
         plt.show()
 
     def _get_chop2_cut(self, chop2_phase):
-        return (chop2_phase * self.Chop2_cut_fn[0] + self.Chop2_cut_fn[1]) + self.CutOffset
+        return (chop2_phase * self.Chop2_cut_fn[0] + self.Chop2_cut_fn[1])/1000 + self.CutOffset
 
 
     def _extract_cd_values(self, log_values, flip_atten=False):
@@ -168,7 +201,7 @@ class Direct_Beam:
 
         return Cd_thickness
     
-    def _trim_and_chop(self, T, I, E, DTC, chop2_phase, idx=0):
+    def _trim_and_chop(self, T, I, E, DTC, chop2_phase, lowest = False):
 
         # trim off points that drop below the intensity threshold
         p = np.where(I >= self.Icut)
@@ -179,15 +212,15 @@ class Direct_Beam:
 
         #trim based on chop2 phase
         TCut = self._get_chop2_cut(chop2_phase)
+        TCut = TCut * 1000
         p = np.where(T >= TCut)
-        print(TCut, chop2_phase, T[2])
         T=T[p]
         I=I[p]
         E=E[p]
         DTC=DTC[p]
 
         # trim off parts above the DTC threshold
-        if idx == 0:
+        if lowest:
             DTC_threshold = self.DTCcut_config1
         else:
             DTC_threshold = self.DTCcut