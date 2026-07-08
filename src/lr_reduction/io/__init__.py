c4f00cf36589854654829d1d52c7e0f1fe94243b
Kevin A. Tactac <tactacka@ornl.gov>
2026-07-08 12:59:33 -0400

    rename loader to interfaces

--- src/lr_reduction/io/__init__.py
+++ src/lr_reduction/io/__init__.py
@@ -1,4 +1,4 @@
-from lr_reduction.io.loader import ConfigLoaderInterface, Loader, RunLoaderInterface
+from lr_reduction.io.interfaces import ConfigLoaderInterface, Loader, RunLoaderInterface
 from lr_reduction.io.run_data import RunData
 
 __all__ = [
