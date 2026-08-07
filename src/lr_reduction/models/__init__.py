efbb8640c36eea97ad67bc5edc8c0982296f22e0
Glass <glass.ships@protonmail.com>
2026-08-07 15:21:54 -0400

    rebase, update test

---
+++ src/lr_reduction/models/__init__.py
@@ -0,0 +1,14 @@
+from lr_reduction.models.config import AssemblyConfig, ReductionConfig, ReflectedRunConfig
+from lr_reduction.models.results import CombinedReductionResult, ReductionResult, ReflectivityCurve
+from lr_reduction.models.run_data import RunData
+
+__all__ = [
+    "AssemblyConfig",
+    "CombinedReductionResult",
+    "ReductionConfig",
+    "ReductionResult",
+    "ReflectedRunConfig",
+    "ReflectedRunConfig",
+    "ReflectivityCurve",
+    "RunData",
+]
--- tests/unit/io/test_orso.py
+++ tests/unit/io/test_orso.py
@@ -4,8 +4,7 @@ import numpy as np
 from orsopy.fileio import Software
 
 from lr_reduction.io.orso import write
-from lr_reduction.models.config import ReductionConfig
-from lr_reduction.models.results import ReductionResult
+from lr_reduction.models import ReductionConfig, ReductionResult, ReflectivityCurve
 
 
 def test_read_orso(template_dir): ...
@@ -19,9 +18,16 @@ def test_write_orso(tmp_path):
     """Test writing an ORSO file from a ReductionResult."""
     config = ReductionConfig()
     reduction_result = ReductionResult(
-        reflectivity=np.array([0.1, 0.2, 0.3]),
+        curve=ReflectivityCurve(
+            q=np.array([0.1, 0.2, 0.3]),
+            r=np.array([0.1, 0.2, 0.3]),
+            dr=np.array([0.01, 0.02, 0.03]),
+            dq=np.array([0.01, 0.02, 0.03]),
+        ),
+        run_numbers=[1, 2, 3],
+        sequence_id="1",
+        sequence_number=1,
         reduction_config=config,
-        html_report=None,
         lr_reduction_info=Software(name="lr_reduction", version="1.0.0"),
         mantid_info=Software(name="mantid", version="1.0.0"),
         reduction_timestamp=datetime.fromisoformat("2024-01-01T12:00:00"),
